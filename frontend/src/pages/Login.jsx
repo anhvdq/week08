f5f2d8de07b08a59a766454a16e71ba9f4c35d51
anhvdq <anh.vdq@gmail.com>
2026-09-16 22:02:46 +1000

    chore: update login title

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -93,7 +93,7 @@ const Login = () => {
             variant="h4"
             gutterBottom
           >
-            KoalaTech University
+            KoalaTech University - Test Updated Workflow
           </Typography>
 
           <Typography
